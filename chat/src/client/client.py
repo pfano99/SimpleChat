1d3f6f22ef6c82ef431b895b9be043006a098da2
unknown <pfanosigama8@gmail.com>
2022-12-17 14:13:32 +0200

    move to class based

---
+++ chat/src/client/client.py
@@ -0,0 +1,82 @@
+import socket
+import concurrent.futures
+import json
+
+with open("../config/config.json") as config_file:
+    data = json.load(config_file)
+
+
+# HOST = data["SERVER_IP"]
+# PORT = data["SERVER_PORT"]
+
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.connect((HOST, PORT))
+
+
+#
+# def receive_message():
+#     while True:
+#         buffer_size = client_socket.recv(data["BUFFER_SIZE"]).decode("utf-8")
+#         recv_msg = client_socket.recv(int(buffer_size.strip())).decode("utf-8")
+#         msg = "{}: {}".format(recv_msg[:recv_msg.index("__")], recv_msg[recv_msg.index("__") + 2:])
+#         print(msg)
+
+
+# def send_username(username: str):
+#     client_socket.send(username.encode("utf-8"))
+#
+
+# def main():
+#     print("[Connected to server]")
+#
+#     username = str(input("Enter username: "))
+#     send_username(username=username)
+#
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+#         _received = executor.submit(receive_message)
+#
+#         while True:
+#             msg = str(input(">> ")).strip()
+#             buffer_size = "{:<8}".format(len(msg))
+#             if int(buffer_size) > 1:
+#                 client_socket.send(buffer_size.encode("utf-8"))
+#                 client_socket.send(msg.encode("utf-8"))
+
+
+class Client:
+    def __init__(self, server_address="127.0.0.1", server_port=5050, buffer_size=1028):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((server_address, server_port))
+        self.buffer_size = buffer_size
+
+    def start(self):
+
+        print("[Connected to server]")
+
+        username = str(input("Enter username: "))
+        self._send_username(username=username)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            _received = executor.submit(self._receive_message)
+
+            while True:
+                msg = str(input(">> ")).strip()
+                buffer_size = "{:<8}".format(len(msg))
+                if int(buffer_size) > 1:
+                    self.client_socket.send(buffer_size.encode("utf-8"))
+                    self.client_socket.send(msg.encode("utf-8"))
+
+    def _send_username(self, username: str):
+        self.client_socket.send(username.encode("utf-8"))
+
+    def _receive_message(self, ):
+        while True:
+            buffer_size = self.client_socket.recv(data["BUFFER_SIZE"]).decode("utf-8")
+            recv_msg = self.client_socket.recv(int(buffer_size.strip())).decode("utf-8")
+            msg = "{}: {}".format(recv_msg[:recv_msg.index("__")], recv_msg[recv_msg.index("__") + 2:])
+            print(msg)
+
+
+if __name__ == "__main__":
+    cl = Client(data["SERVER_IP"], data["SERVER_PORT"], data["BUFFER_SIZE"])
+    cl.start()
---
+++ chat/src/server/server.py
@@ -0,0 +1,142 @@
+import concurrent.futures
+import json
+import socket
+import threading
+
+with open("../config/config.json") as config_file:
+    data = json.load(config_file)
+
+# HOST = data["SERVER_IP"]
+# PORT = data["SERVER_PORT"]
+
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_socket.bind((HOST, PORT))
+# server_socket.listen()
+
+# connected_clients = []/
+
+threadLock = threading.Lock()
+
+
+# def _handle_message(_message, _from):
+#     _to = str(_message.decode("utf-8"))[:str(_message.decode("utf-8")).index("__")]
+#     threadLock.acquire()
+#     for client in connected_clients:
+#         if _from is client["connection"]:
+#             _message = str(_message.decode("utf-8")).replace(_to, client["username"]).encode("utf-8")
+#
+#     for client in connected_clients:
+#         if str(client["username"]).strip().lower() == _to.lower():
+#             buffer_size = "{:<8}".format(len(_message.decode("utf-8")))
+#             client["connection"].send(buffer_size.encode("utf-8"))
+#             client["connection"].send(_message)
+#     threadLock.release()
+
+#
+# def _handle_connection(conn, addr):
+#     print("[Server accepted new connection from {}]....".format(addr))
+#     is_username_set = False
+#     while True:
+#         if not is_username_set:
+#             username = conn.recv(data["USERNAME_BUFFER_SIZE"]).decode("utf-8")
+#             threadLock.acquire()
+#             for client in connected_clients:
+#                 if client["connection"] is conn:
+#                     client["username"] = username
+#             threadLock.release()
+#             is_username_set = True
+#
+#         buffer_size = int(conn.recv(data["BUFFER_SIZE"]).decode("utf-8"))
+#         message = conn.recv(buffer_size)
+#         _handle_message(message, conn)
+
+#
+# def _listen_for_connection():
+#     while True:
+#         print("[Server listening for connections].... ")
+#         conn, addr = server_socket.accept()
+#         connected_clients.append(
+#             {
+#                 "connection": conn,
+#                 "address": addr,
+#                 "username": None
+#             }
+#         )
+#         print("[New connection established]....")
+#
+#         conn_thread = threading.Thread(target=_handle_connection, args=(conn, addr))
+#         conn_thread.start()
+
+
+# def main():
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+#         print("[Server started]....")
+#         _listen = executor.submit(_listen_for_connection)
+
+
+class Server:
+    def __init__(self, host="127.0.0.1", port=5050, buffer_size=1028, username_buffer_size=50):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((host, port))
+        self.server_socket.listen()
+
+        self.connected_clients = []
+        self.buffer_size = buffer_size
+        self.username_buffer_size = username_buffer_size
+
+    def start(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            print("[Server started]....")
+            _listen = executor.submit(self._listen_for_connection)
+
+    def _listen_for_connection(self):
+        while True:
+            print("[Server listening for connections].... ")
+            conn, addr = self.server_socket.accept()
+            self.connected_clients.append(
+                {
+                    "connection": conn,
+                    "address": addr,
+                    "username": None
+                }
+            )
+            print("[New connection established]....")
+
+            conn_thread = threading.Thread(target=self._handle_connection, args=(conn, addr))
+            conn_thread.start()
+
+    def _handle_connection(self, conn, addr):
+        print("[Server accepted new connection from {}]....".format(addr))
+        is_username_set = False
+        while True:
+            if not is_username_set:
+                username = conn.recv(self.username_buffer_size).decode("utf-8")
+                threadLock.acquire()
+                for client in self.connected_clients:
+                    if client["connection"] is conn:
+                        client["username"] = username
+                threadLock.release()
+                is_username_set = True
+
+            buffer_size = int(conn.recv(self.buffer_size).decode("utf-8"))
+            message = conn.recv(buffer_size)
+            self._handle_message(message, conn)
+
+    def _handle_message(self, _message, _from):
+        _to = str(_message.decode("utf-8"))[:str(_message.decode("utf-8")).index("__")]
+        threadLock.acquire()
+        for client in self.connected_clients:
+            if _from is client["connection"]:
+                _message = str(_message.decode("utf-8")).replace(_to, client["username"]).encode("utf-8")
+
+        for client in self.connected_clients:
+            if str(client["username"]).strip().lower() == _to.lower():
+                buffer_size = "{:<8}".format(len(_message.decode("utf-8")))
+                client["connection"].send(buffer_size.encode("utf-8"))
+                client["connection"].send(_message)
+        threadLock.release()
+
+
+if __name__ == "__main__":
+    sev = Server(data["SERVER_IP"], data["SERVER_PORT"], data["BUFFER_SIZE"], data["BUFFER_SIZE"])
+    sev.start()
